3b91d5aa80e23d92e252590366821e1f87a90636
Pablo Fernandes <pablofsilva91@gmail.com>
2017-08-02 13:55:11 -0300

    Aula 01 -Encapsulamento e Modificadores de Acesso

---
+++ OrientacaoObjetos/CaixaEletronico/Conta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Conta
+    {
+        public int Numero { get; set;}
+        public double Saldo {get; private set;}
+
+        public Cliente Titular { get; set; }
+
+        public void Saca(double valor)
+        {
+            this.Saldo -= valor;
+        }
+
+        public void Deposita (double valor)
+        {
+            this.Saldo += valor;
+        }
+    }
+}
