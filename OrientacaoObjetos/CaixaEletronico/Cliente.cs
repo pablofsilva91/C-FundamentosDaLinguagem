c149592b6cdac6b271064a22a89ce0aed19053f2
Pablo Fernandes <pablofsilva91@gmail.com>
2017-08-03 13:35:40 -0300

    Aula 02 -  Construtores

--- OrientacaoObjetos/CaixaEletronico/Cliente.cs
+++ OrientacaoObjetos/CaixaEletronico/Cliente.cs
@@ -8,5 +8,16 @@ namespace CaixaEletronico
 {
     class Cliente
     {
+        public string Nome { get; set; }
+
+        public Cliente (string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public Cliente()
+        {
+
+        }
     }
 }
