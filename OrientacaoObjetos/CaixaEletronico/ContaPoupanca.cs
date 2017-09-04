a99bb41f6dbfe73dc0d158e358e16902737d0e42
Pablo Fernandes <pablofsilva91@gmail.com>
2017-09-04 13:36:46 -0300

    Aula 10 - Interfaces

--- OrientacaoObjetos/CaixaEletronico/ContaPoupanca.cs
+++ OrientacaoObjetos/CaixaEletronico/ContaPoupanca.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace CaixaEletronico
 {
-    class ContaPoupanca : Conta 
+    class ContaPoupanca : Conta, ITributavel 
     {
         public override void Deposita(double valor)
         {
             this.Saldo += (valor + 0.10);
         }
         
+        public double CalculaTributo()
+        {
+            return this.Saldo * 0.02;
+        }
 
         
     }
