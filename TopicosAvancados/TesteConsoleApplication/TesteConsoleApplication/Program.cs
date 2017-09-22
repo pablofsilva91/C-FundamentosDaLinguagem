351d01a7cad09f30b1a4d7be0fb63e6a7808edca
Pablo <pablofsilva91@gmail.com>
2017-09-22 00:31:05 -0300

    Leitura de dados do terminal

---
+++ TopicosAvancados/TesteConsoleApplication/TesteConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma frase. Pressione Ctrl+C para sair");
+            TextReader leitor = Console.In;
+            string linha = leitor.ReadLine();
+            while (linha != null)
+            {
+                linha = leitor.ReadLine();
+            }
+        }
+    }
+}
